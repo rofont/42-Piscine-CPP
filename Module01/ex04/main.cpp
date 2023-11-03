34800820a823ea5ceba6800f4787e6c931957f95
Romain FONTAINE <fontaine.romain@gmx.com>
2023-11-03 15:52:29 -0400

    ex04 in progress

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -1,35 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ZombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 09:37:31 by rofontai          #+#    #+#             */
-/*   Updated: 2023/11/03 14:06:24 by romain           ###   ########.fr       */
+/*   Created: 2023/11/03 13:50:19 by romain            #+#    #+#             */
+/*   Updated: 2023/11/03 15:52:02 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using std::cout;
 using std::cerr;
+using std::endl;
+using std::string;
 
-Zombie*	zombieHorde(int N, std::string name)
+int	main(int ac, char **av)
 {
-	if (N < 1)
+	if (ac != 4)
 	{
-		cerr << "The numbers of Zombie is not accepted" <<endl;
+		cerr << "🚨 ./replace <filename> <string1> <string2>" << endl;
 		exit(EXIT_FAILURE);
 	}
-	if (name.empty())
+
+	string name_file = av[1];
+	string current = av[2];
+	string replace = av[3];
+
+	std::fstream myFlux(name_file);
+	if (!myFlux)
 	{
-		cerr << "The Zombie have not name" <<endl;
+		cerr << "🚨 This file is wrong or you don't have acces" << endl;
 		exit(EXIT_FAILURE);
 	}
-	Zombie *myHordeZombie = new Zombie[N];
 
-	for(int i = 0; i < N; i++)
-		myHordeZombie[i].setName(name);
 
-	return myHordeZombie;
+
+
+	return 0;
 }
