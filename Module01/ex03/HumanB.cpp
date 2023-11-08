c46b5d482e60846d046bb04d6839382168843c3b
rofont <fontaine.romain@gmx.com>
2023-11-08 11:34:42 -0500

    cpp02 ex 00 finish

--- Module01/ex03/HumanB.cpp
+++ Module01/ex03/HumanB.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romain <romain@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rofontai <rofontai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 11:00:08 by rofontai          #+#    #+#             */
-/*   Updated: 2023/11/03 13:19:07 by romain           ###   ########.fr       */
+/*   Updated: 2023/11/08 09:25:22 by rofontai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack(void) const
 {
-	//gérer si pas d'arme
-	cout << _name << " attacks with their " << _weapon->getType() << endl;
+	if (_weapon)
+		cout << _name << " attacks with their " << _weapon->getType() << endl;
 }
---
+++ Module02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rofontai <rofontai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 10:38:38 by rofontai          #+#    #+#             */
+/*   Updated: 2023/11/08 11:30:06 by rofontai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	a.setRawBits(12);
+	Fixed c;
+	
+	c = b;
+	c.setRawBits(5);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
